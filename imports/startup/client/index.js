1c02f1c1f5ed4aae8eb56380565ed3bd38faa8fe
Sean Hong <iamseanhong@gmail.com>
2018-05-13 17:47:34 -0700

    Connect Apollo to React

--- imports/startup/client/index.js
+++ imports/startup/client/index.js
@@ -2,8 +2,29 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Meteor } from 'meteor/meteor';
 
+import { ApolloClient } from 'apollo-client';
+import { HttpLink, InMemoryCache } from 'apollo-client-preset';
+import { ApolloProvider } from 'react-apollo';
+
 import App from '../../ui/App';
 
+const httpLink = new HttpLink({
+  uri: Meteor.absoluteUrl('graphql'),
+});
+
+const cache = new InMemoryCache();
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache,
+});
+
+const ApolloApp = () => (
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+);
+
 Meteor.startup(() => {
-  render(<App />, document.getElementById('app'));
+  render(<ApolloApp />, document.getElementById('app'));
 });
