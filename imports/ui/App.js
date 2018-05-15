56315f416655f11a761d9730d9534dc36b9790b6
Sean Hong <iamseanhong@gmail.com>
2018-05-14 20:30:01 -0700

    Able to render resolutions with new query (data.loading)

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -3,9 +3,15 @@ import gql from 'graphql-tag';
 import { graphql } from 'react-apollo';
 
 const App = ({ data }) => {
+  if (data.loading) return null;
   return (
     <div>
       <div>{data.hi}</div>
+      <ul>
+        {data.resolutions.map(resolution => {
+          return <li key={resolution._id}>{resolution.name}</li>;
+        })}
+      </ul>
     </div>
   );
 };
@@ -13,6 +19,10 @@ const App = ({ data }) => {
 const hiQuery = gql`
   {
     hi
+    resolutions {
+      _id
+      name
+    }
   }
 `;
 
